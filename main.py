51e4f13ae6629d08d2d648bfb2dbe8ea3d65db76
artyom <art.volyk@gmail.com>
2020-11-16 15:27:55 +0200

    repair code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import requests
 import telegram
 from dotenv import load_dotenv
+from pprint import pprint
 
 
 def main():
@@ -20,17 +21,18 @@ def main():
 
     while True:
         try:
-            response = requests.get('https://dvmn.org/api/long_polling/', headers=headers, params=params)
+            response = requests.get('https://dvmn.org/api/long_polling/', headers=headers, params=params, timeout=91)
             response.raise_for_status()
         except (requests.exceptions.ReadTimeout, ConnectionError):
+            time.sleep(3)
             continue
         response = response.json()
-        new_attempts = response['new_attempts'][0]
         if response['status'] == 'timeout':
             params['timestamp'] = response['timestamp_to_request']
         elif response['status'] == 'found':
-            lesson = new_attempts['lesson_title']
-            if new_attempts['is_negative']:
+            new_attempt = response['new_attempts'][0]
+            lesson = new_attempt['lesson_title']
+            if new_attempt['is_negative']:
                 bot.send_message(chat_id=chat_id, text=f'У вас проверили работу "{lesson}"\n\nК сожалению, в '
                                                        f'работе нашлись ошибки.')
             else:
